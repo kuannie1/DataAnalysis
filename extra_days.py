3a54193534332e8738ed3f3775f3a65b6a3d9ceb
kuannie1 <anne.ku4@gmail.com>
2016-04-13 21:46:51 -0400

    Code for webscraping, sentiment-analyzing, and date-generating

--- extra_days.py
+++ extra_days.py
@@ -4,20 +4,14 @@ from pattern.web import URL, extension, download
 import pickle
 from selenium import webdriver
 
-#Storing all the days when stock market closes
-fin = open('list_of_days.pickle')
-complete_list_of_dates = pickle.load(fin)
-fin.close()
-print datetime.now()
-
 driver = webdriver.Firefox()
-
+dates = [(12, 29, 2014), (12, 30, 2014), (12, 31, 2014), (1, 1, 2015), (1, 2, 2015), (1, 3, 2015), (1, 4, 2015), (1, 5, 2015), (1, 6, 2015), (1, 7, 2015), (1, 8, 2015), (1, 9, 2015), (1, 10, 2015)]
 #For loop to iterate through the days:
-for date in complete_list_of_dates:
+for date in dates:
 	month = date[0]
 	day = date[1]
 	year = date[2]
-
+	print month, day, year
 	result = []
 	f = open('{}_{}_{}googleresults.pickle'.format(month, day, year), 'w')
 	driver.get('https://www.google.com/#q=Chipotle+news&tbs=cdr%3A1%2Ccd_min%3A{}%2F{}%2F{}%2Ccd_max%3A{}%2F{}%2F{}&tbm='.format(month, day-1, year, month, day, year))
@@ -29,7 +23,3 @@ for date in complete_list_of_dates:
 	f.close()
 
 driver.close()
-
-
-
-
--- sentimentanalysis.py
+++ sentimentanalysis.py
@@ -6,38 +6,72 @@ from keys import indico_api_key
 indicoio.config.api_key = indico_api_key
 
 
-Jan_days = [2, 5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 26, 27, 28, 29, 30]
+#Loading all the days for naming in the daily_sentiments_function() function
+fin = open('list_of_days.pickle')
+complete_list_of_dates = pickle.load(fin)
+fin.close()
+#Changing the directory so that the sentiment pickle file is stored with all the google results 
+import os
 
-def January_sentiments():
-	daily_sentiments = []
-	for day in Jan_days:
+path = "/home/anne/DataAnalysis/2016_Google_Results"
+
+# Check current working directory.
+retval = os.getcwd()
+print "Current working directory %s" % retval
+
+# Now change the directory
+os.chdir( path )
+
+# Check current working directory.
+retval = os.getcwd()
+
+print "Directory changed successfully %s" % retval
+
+def daily_sentiments_function():
+	daily_sentiments_dictionary = {}
+	for date in complete_list_of_dates:
+		year = date[2]
+		month = date[0]
+		day = date[1]
+
 		#loading the results for interpretation
-		fin = open('Jan{}_results.pickle'.format(day))
-		result_list = pickle.load(fin)
-		fin.close()
+		fin1 = open('{}_{}_{}googleresults.pickle'.format(month, day, year))
+		list_of_results = pickle.load(fin1)
+		fin1.close()
 
 		# For storing the sentiments, 
 		sentiments = []
-		sentiment_list_length = len(sentiments)
+		# sentiment_list_length = len(sentiments)
 
 		#looping and appending sentiment results to the list
-		for result in result_list:
+		for result in list_of_results:
 			sentiments.append(indicoio.sentiment(result))
 		sentiment_list_length = len(sentiments)
 
 		sentiment_summation = 0
+
 		#finding the average sentiment for this particular day
 		for i in range(sentiment_list_length):
 			sentiment_summation += sentiments[i]
-			sentiment_avg = sentiment_summation/ sentiment_list_length
-		daily_sentiments.append(sentiment_avg)
+			sentiment_avg = sentiment_summation/sentiment_list_length
+			daily_sentiments_dictionary[month, day, year] = sentiment_avg
+	return daily_sentiments_dictionary
 
-	return daily_sentiments #the list, with one sentiment value/day
+dictionary_of_greatness = daily_sentiments_function()
+f = open('sentiments.pickle', 'w')
+pickle.dump(dictionary_of_greatness, f)
+f.close()
 
-print January_sentiments()
-		
+	#Create dictionary with date leading to sentiment
 
 
+# f = open('{}_sentiments.pickle'.format(month), 'w')
+# pickle.dump(daily_sentiments(), f)
+# f.close()
+
+
+
+#Indicoio examples:
 	# print "The Average Sentiment value for day {} is: ".format(day) + str(sentiment_avg)
 
 
